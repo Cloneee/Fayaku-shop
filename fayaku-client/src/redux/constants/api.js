486074e57ef71b063c200e06addcd2ed1b4fc3ea
LuuTuanKha <luutkha@gmail.com>
2021-08-02 08:11:31 +0700

    Update Carousel
    + Just change Carousel desgin
    + Not update image link yet

--- fayaku-client/src/redux/constants/api.js
+++ fayaku-client/src/redux/constants/api.js
@@ -1,5 +1,5 @@
 export const ONE_PRODUCT_API = 'http://localhost:8080/projectperfume/api/product/'
-export const ALL_PRODUCT_API = 'http://localhost:8080/projectperfume/api/products'
+export const ALL_PRODUCT_API = 'http://localhost:8000/api/products'
 
 
 
