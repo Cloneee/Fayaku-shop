411bcece26fe85e176cb49c26e57cb60236b7160
Huy <40709168+Cloneee@users.noreply.github.com>
2021-08-27 07:45:02 +0700

    Update tool crawling

--- getDataGoodsmile.js
+++ getDataGoodsmile.js
@@ -11,27 +11,36 @@ const copyToClipboard = str => {
     document.body.removeChild(el);
 };
 const getData = () => {
-    let imgString = ''
+    let imgString = []
     $('img[class=itemImg]').each((i,v)=>{
-        if (i==0){
-            imgString = v.src
-        } else{
-            imgString = imgString + ',' + v.src
-        }
+        imgString.push(v.src)
     })
     let name = $("dt:contains('商品名')").next().text()
-    let producer = $("dt:contains('メーカー')").next().text()
+    let description = $('div[class="description"] p').text()
+    let brand = $("dt:contains('メーカー')").next().text()
     let category = $("dt:contains('カテゴリー')").next().text()
     let price = $("dt:contains('価格')").next().text()
+    let release = $("dt:contains('発売時期')").next().text()
+    let detail = $("dt:contains('仕様')").next().text()
     name = name.replace(/\s\\n+/g, '').trim()
-    producer = producer.replace(/\s\\n+/g, '').trim()
+    brand = brand.replace(/\s\\n+/g, '').trim()
     category = category.replace(/\s\\n+/g, '').trim()
+    release = release.replace(/\s\\n+/g, '').trim()
+    detail = detail.replace(/\s\\n+/g, '').trim()
     price = price.match(/\d/g)
     price = price.join('')
     price = +price
-    requestString = `name=${name}&producer=${producer}&category=${category}&price=${price}&image=${imgString}`
-    copyToClipboard(requestString)
+    let jsonData = {
+        name: name,
+        description: description,
+        brand: brand,
+        category: category,
+        price: price,
+        release: release,
+        detail: detail,
+        image: imgString
+    }
+    copyToClipboard(JSON.stringify(jsonData))
 }
 
 getData()
-
